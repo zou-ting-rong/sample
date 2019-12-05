755ce10bf98a2df7f57d1bdaf0263a25e7584e23
zou-ting-rong <1198875194@qq.com>
2019-12-05 14:23:12 +0800

    完整的单链表基本操作

--- linklist1.c
+++ linklist1.c
@@ -40,11 +40,43 @@ void display(list *head)
 	}
 	printf("\n");
 }
+
+int insert(list *head,int j)
+{
+	list *temp = head;
+	for(int i =0;i < j;i++){
+		temp = temp->next;
+	}
+	//temp->data = j;
+	list *a;
+	a = (list *)malloc(sizeof(list));
+	a->next = temp->next;
+	a->data = j;
+	temp->next = a;
+	printf("插入成功！\n");
+}
+
+int del(list *head,int j)
+{
+	list *temp = head;
+	for(int i = 1;i < j;i++){
+		temp = temp->next;
+	}
+	list *c;
+	c = (list *)malloc(sizeof(list));
+	c = temp->next;
+	temp->next = temp->next->next;
+	free(c);
+}
 int main(int argc ,char **argv)
 {
 	list *head;
 	head = (list *)malloc(sizeof(list));
 	head = Init();
+	display(head);
+	insert(head,2);
+	display(head);
+	del(head,2);
 	display(head);
 	return 0;
 }
